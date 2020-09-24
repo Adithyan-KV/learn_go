3d1314a3f08bf15657b7ff468ac7eba757304368
Adithyan-KV <adithyankv007@gmail.com>
2020-09-24 22:48:44 +0530

    played around with some basic stuff

---
+++ variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hello string = "Hello"
+	var number uint8 = 255
+	var new_number uint8 = 200
+	var sum uint8 = number + new_number
+	fmt.Println(sum)
+	fmt.Println(hello)
+	fmt.Println(number + new_number)
+}
